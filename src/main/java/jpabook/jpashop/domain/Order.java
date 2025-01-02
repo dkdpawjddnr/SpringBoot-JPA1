3d071f9632aea3ce4e52a492f6d52682c1571147
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-01-02 23:49:32 +0900

    엔티티 클래스 개발3

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id") // 외래키 이름이 member_id가 됨.
     private Member member;
 
-    @OneToMany(mappedBy = "order") // OrderItem의 order 변수
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // OrderItem의 order 변수
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)//casecade는 order저장할 때 delivery엔티티도 계속 persist해준다.
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,4 +34,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+    //==연관관계 편의 메소드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
